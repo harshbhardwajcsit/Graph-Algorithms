cdb72e86c4e4fbe743c34b050948324f408cab09
Harsh Bhardwaj <hbhardwaj@ptc.com>
2017-06-20 15:40:34 +0530

    greedysearch

---
+++ GreedySearch_part1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package greedysearch_part1;
+
+/**
+ *
+ * @author hbhardwaj
+ */
+public class GreedySearch_part1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
